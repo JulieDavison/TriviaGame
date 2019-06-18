cd511cf3cd2c2537a1a9e4b41ed95717d29bf764
Julie Davison <davison.jules@gmail.com>
2019-06-18 14:25:01 -0700

    psudocode js

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+
+//    Make object to hold questions, wrong answers, correct answer, picture.
+var QandA = [
+    {question: "Question here", correct: "answer here", choices: ["choice1", "choice2", "choice3"], imagefile:"",}
+
+]
+
+
+
+//    Global Variable:
+
+//    Make function to ask questions in a random order and includes all the questions each round.
+//          Math.floor((Math.random() * 10) + 1); change to random index in Object.
+    
+//    Make function that resets timer for each question and the game.  Keeping track of correct, incorrect and unanswered questions for final page.
+
+//    Create html and css to display questions and final page scoring plus replay game button.
+
+//    Use if and if/else statements to determine if the questions have been answered correctly.
