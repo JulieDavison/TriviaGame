553c91e58e7c9716bf4fff9849a625441e99122a
Julie Davison <davison.jules@gmail.com>
2019-06-18 19:54:32 -0700

    added images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,48 @@
 
 //    Make object to hold questions, wrong answers, correct answer, picture.
 var QandA = [
-    {question: "Question here", correct: "answer here", choices: ["choice1", "choice2", "choice3"], imagefile:"",}
+    {
+        question: "Which of these AVAs is in both Napa and Sonoma County?", choices: ["Carneros", "Knights Valley", "All of these", "None of these"], correct: 0, imagefile: "assets.images.AVA-Map1.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.Buds-Bacigalupi-oldvines.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
+    {
+        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+    },
 
-]
-
-
+];
+console.log(QandA.choices[0]);
 
 //    Global Variable:
 
 //    Make function to ask questions in a random order and includes all the questions each round.
-//          Math.floor((Math.random() * 10) + 1); change to random index in Object.
+
+        // var randomQandA = QandA[Math.floor(Math.random() * QandA.length); 
     
+    // change to random index in Object.
+
 //    Make function that resets timer for each question and the game.  Keeping track of correct, incorrect and unanswered questions for final page.
 
 //    Create html and css to display questions and final page scoring plus replay game button.
