0515b7cce77ba91f81de65785b91936bafb1b5af
Julie Davison <davison.jules@gmail.com>
2019-06-21 20:52:06 -0700

    html and js theough quest display and timer stop on userclick

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,143 @@
+$(document).ready(function () {
 
-//    Make object to hold questions, wrong answers, correct answer, picture.
-var QandA = [
-    {
-        question: "Which of these AVAs is in both Napa and Sonoma County?", choices: ["Carneros", "Knights Valley", "All of these", "None of these"], correct: 0, imagefile: "assets.images.AVA-Map1.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.Buds-Bacigalupi-oldvines.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
-    {
-        question: "Question here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
-    },
 
-];
-console.log(QandA.choices[0]);
 
-//    Global Variable:
+
+
+    //    Make object to hold questions, wrong answers, correct answer, picture.
+    var QandA = [
+        {
+            question: "Which of these AVAs is in both Napa and Sonoma County?", choices: ["Carneros", "Knights Valley", "All of these", "None of these"], correct: 0, imagefile: "assets.images.AVA-Map1.jpg",
+        },
+        {
+            question: "Question2 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.Buds.jpg",
+        },
+        {
+            question: "Question3 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question4 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question5 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question6 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question7 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question8 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question9 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+        {
+            question: "Question10 here", choices: ["choice1", "choice2", "choice3", "choice4"], correct: 1, imagefile: "assets.images.800px-Grapes.jpg",
+        },
+
+    ];
+    // console.log(QandA.choices[0]);
+
+    //    Global Variable:
     var correct = 0
     var incorrect = 0
     var unanswered = 0
 
+    var index;
+    var pick = [];
+    var timeRemaining = 20;
+    var timerRunning;
+    var userChoicesIndex = [];
+    // ---------------Functions----------------------------
 
+    // Start-game button is clicked to begin questions.
+    $("#startButton").on("click", function () {
+        $("#startButton").hide();
+        timerGenerator();
+        startNewQuest();
 
-//    Make function to ask questions in a random order and includes all the questions each round.
+        function timerGenerator() {
+            timerRunning = setInterval(function(){
+                if(timeRemaining === 0){
+                    clearInterval(timerRunning);
+                    //Move on to wrong answer
+                }
+                $("#timedisplay").text(timeRemaining);
+                timeRemaining--;
 
-        // var randomQandA = QandA[Math.floor(Math.random() * QandA.length); 
-        // function to make sure all questions get asked
+            }, 1000);
+
+        }
+        // console.log("timer");
+
+        // Make function to ask questions in a random order and includes all the questions each round.
         // if question has been asked then 
         // for loop to loop through questions and if/else to verify question
-    
-    // change to random index in Object.
+        function startNewQuest() {
+            randomQandA = Math.floor(Math.random() * QandA.length);
+            // check to see if randonQandA is in new array hold questions asked
+            // then let pick be a new question not in array.
+            pick = QandA[randomQandA];
+            console.log(pick);
 
-//    Make function that resets timer for each question and the game.  Keeping track of correct, incorrect and unanswered questions for final page.
-        // function to start Game
-            // start new game
-            // start timer
-            // display first question
+            // Display "pick" in jumbotron
+            $("#question").text(pick.question);
 
-        // function to start each question
-            // reset timer
-            // display new question
-            // record scorefrom the previous question win++ or lose++
 
-//    Create html and css to display questions and 
-    // final page scoring plus replay game button.
+            var ulEl = $('<ul>').addClass('list-group');
 
-//    Use if and if/else statements to determine if the questions have been answered correctly.
+            for (var i = 0; i < pick.choices.length; i++) {
+                var userChoice = $("<li>");
+                userChoice.addClass("answerchoice")
+                          .addClass("list-group-item")
+                          .attr('id', 'choice' + (i + 1))
+                          .attr("data-guessvalue", i);
+                userChoice.text(pick.choices[i]);
+                ulEl.append(userChoice)
+                //assign array position to it so can check answer
+            }
+            $("#answerBlock").append(ulEl);
+            
+            //click function to select answer and outcomes
+            $(".answerchoice").on("click", function () {
+                console.log('I Work')
+                //Clear timer interval so we do not get unwanted results
+                clearInterval(timerRunning);
+                //grab array position from userGuess
+                userGuess = parseInt($(this).attr("data-guessvalue"));
+
+                if (userChoice === pick.correct[i]) {
+                    correct++;
+                    // display imagefile for 7 secs then display next question;
+                    // $("#imgDiv").html(src, "imagefile");
+                }
+                else if (userChoice !== pick.correct[i]) {
+                    incorrect++;
+                    // display imagefile for 7 secs then display next question;
+                    // $("#imgDiv").html(src, "imagefile");
+                }
+                else {
+                    unanswered++;
+                    // display imagefile for 7 secs then display next question (function startNewQuest);
+                    // $("#imgDiv").html(src, "imagefile");
+                }
+
+
+            });
+        };
+        if (pick === null) {
+            //Display final page
+            $("#").empty();
+            $("#").html("<h2>Game Over!  Let's see how you did.</h2>");
+            $("#").append("<h4> Correct: " + correct + "</h4>" );
+            $("#").append("<h4> Correct: " + incorrect + "</h4>" );
+            $("#").append("<h4> Correct: " + unanswered + "</h4>" );
+            // Show Start-Game button again.
+        }
+
+
+
+    });
+});
